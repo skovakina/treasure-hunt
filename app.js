ec9af3b4aa44cb26492e034530c1a5f28ec7faa4
Svetlana Kovakina <skovakina@gmail.com>
2025-02-24 11:24:31 -0800

    feat: render grid

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("hello world");
+const boardSize = 10;
+const gemCount = 3;
+const board = document.querySelector(".game-board");
+
+let gemPositions = [];
+
+const placeGemsRandomly = () => {
+  while (gemPositions.length < gemCount) {
+    const randomRow = Math.floor(Math.random() * boardSize);
+    const randomCol = Math.floor(Math.random() * boardSize);
+    const position = `${randomRow}-${randomCol}`;
+    if (!gemPositions.includes(position)) {
+      gemPositions.push(position);
+    }
+  }
+};
+
+function createGrid() {
+  placeGemsRandomly();
+
+  for (let row = 0; row < boardSize; row++) {
+    for (let col = 0; col < boardSize; col++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      const position = `${row}-${col}`;
+
+      if (gemPositions.includes(position)) {
+        cell.classList.add("gem");
+      }
+      board.appendChild(cell);
+    }
+  }
+}
+
+createGrid();
