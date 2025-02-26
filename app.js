ff7a5c358abe42a6f1b4472ca7d50095e4fcb833
Svetlana Kovakina <skovakina@gmail.com>
2025-02-26 14:17:17 -0800

    feat: level up

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const BOARD_SIZE = 10;
-const GEM_COUNT = 5;
+const BOARD_SIZE = 5;
+const GEM_COUNT = 1;
 const TIMER = 30;
 
 let timeLeft = TIMER;
 let boardSize = BOARD_SIZE;
+let gemCount = GEM_COUNT;
 let timerInterval;
 let isPaused = false;
 let totalScore = 0;
@@ -11,7 +12,6 @@ let gemsFound = 0;
 let gemPositions = [];
 let cells = [];
 
-// Offsets for hint positions
 const OFFSET = [
   [-1, -1],
   [-1, 0],
@@ -29,144 +29,23 @@ const timerValue = document.querySelector(".timer-value");
 const pauseButton = document.querySelector(".pause-btn");
 const scoreEl = document.querySelector(".score-value");
 const gemEl = document.querySelector(".gem-found");
+const gemTotalEl = document.querySelector(".gem-total");
 const congratulationsEl = document.querySelector(".congratulations");
+const youLostEl = document.querySelector(".you-lost");
 const gameOverEl = document.querySelectorAll(".game-over");
 const gameOverBtn = document.querySelectorAll(".game-over-btn");
 const actions = document.querySelector(".game-actions-container");
+const nextLevelBtn = document.querySelector(".next-level-btn");
 
-gameOverEl.forEach((el) => el.classList.add("hidden"));
+gameOverEl.forEach((el) => hideElement(el));
 
 pauseButton.addEventListener("click", handlePause);
 startButton.addEventListener("click", startGame);
+nextLevelBtn.addEventListener("click", handleLevelUp);
+
 gameOverBtn.forEach((btn) => {
   btn.addEventListener("click", startGame);
 });
-
-function getGemsPositions() {
-  while (gemPositions.length < GEM_COUNT) {
-    const randomRow = Math.floor(Math.random() * boardSize);
-    const randomCol = Math.floor(Math.random() * boardSize);
-    const position = `${randomRow}-${randomCol}`;
-    if (!gemPositions.includes(position)) {
-      gemPositions.push(position);
-    }
-  }
-}
-
-function getHintsPositions() {
-  let hintPosition = [];
-  gemPositions.forEach((gem) => {
-    const [gemRow, gemCol] = gem.split("-").map(Number);
-    OFFSET.forEach((offset) => {
-      const row = gemRow + offset[0];
-      const col = gemCol + offset[1];
-      const position = `${row}-${col}`;
-      if (isValidPosition(row, col) && !gemPositions.includes(position)) {
-        hintPosition.push(position);
-      }
-    });
-  });
-  return hintPosition;
-}
-
-function isValidPosition(row, col) {
-  return row >= 0 && row < boardSize && col >= 0 && col < boardSize;
-}
-
-function createGrid() {
-  getGemsPositions();
-  hints = getHintsPositions();
-
-  for (let row = 0; row < boardSize; row++) {
-    for (let col = 0; col < boardSize; col++) {
-      const cell = new Cell(row, col, onGemFound);
-      cells.push(cell);
-      if (gemPositions.includes(`${row}-${col}`)) {
-        cell.setType("gem");
-      }
-      if (hints.includes(`${row}-${col}`)) {
-        cell.setType("hint");
-      }
-    }
-  }
-}
-
-function clearGrid() {
-  while (board.firstChild) {
-    board.innerHTML = "";
-  }
-}
-
-function startGame() {
-  clearGrid();
-  resetGameState();
-  createGrid();
-
-  cells.forEach((cell) => cell.setEnabled());
-
-  gameOverEl.forEach((el) => el.classList.add("hidden"));
-  congratulationsEl.classList.add("hidden");
-  startButton.classList.add("hidden");
-  actions.classList.remove("hidden");
-
-  startTime();
-}
-
-function resetGameState() {
-  clearGrid();
-  timeLeft = TIMER;
-  timerValue.textContent = TIMER;
-  gemPositions = [];
-  totalScore = 0;
-  gemsFound = 0;
-  scoreEl.textContent = totalScore;
-  gemEl.textContent = gemsFound;
-  isPaused = false;
-}
-
-function startTime() {
-  timerInterval = setInterval(() => {
-    timeLeft--;
-    timerValue.textContent = timeLeft;
-    if (timeLeft === 0) {
-      clearInterval(timerInterval);
-    }
-  }, 1000);
-}
-
-function handlePause() {
-  if (isPaused) {
-    isPaused = false;
-    startTime();
-    cells.forEach((cell) => cell.setEnabled());
-    pauseButton.style.backgroundImage = "url('./assets/icons/icon-pause.png')";
-  } else {
-    clearInterval(timerInterval);
-    isPaused = true;
-    cells.forEach((cell) => cell.setDisabled());
-    pauseButton.style.backgroundImage = "url('./assets/icons/icon-play-s.png')";
-  }
-}
-
-function initialize() {
-  actions.classList.add("hidden");
-  createGrid();
-}
-
-function onGemFound() {
-  totalScore += 100;
-  scoreEl.textContent = totalScore;
-  gemsFound++;
-  gemEl.textContent = gemsFound;
-  if (gemsFound === GEM_COUNT && timeLeft > 0) {
-    clearInterval(timerInterval);
-    congratulationsEl.classList.remove("hidden");
-  }
-  if (timeLeft === 0) {
-    clearInterval(timerInterval);
-    gameOverEl.classList.remove("hidden");
-  }
-}
 
 class Cell {
   constructor(row, col, onGemFound) {
@@ -226,4 +105,146 @@ class Cell {
   }
 }
 
-initialize();
+function getGemsPositions() {
+  while (gemPositions.length < gemCount) {
+    const randomRow = Math.floor(Math.random() * boardSize);
+    const randomCol = Math.floor(Math.random() * boardSize);
+    const position = `${randomRow}-${randomCol}`;
+    if (!gemPositions.includes(position)) {
+      gemPositions.push(position);
+    }
+  }
+}
+
+function getHintsPositions() {
+  let hintPosition = [];
+  gemPositions.forEach((gem) => {
+    const [gemRow, gemCol] = gem.split("-").map(Number);
+    OFFSET.forEach((offset) => {
+      const row = gemRow + offset[0];
+      const col = gemCol + offset[1];
+      const position = `${row}-${col}`;
+      if (isValidPosition(row, col) && !gemPositions.includes(position)) {
+        hintPosition.push(position);
+      }
+    });
+  });
+  return hintPosition;
+}
+
+function isValidPosition(row, col) {
+  return row >= 0 && row < boardSize && col >= 0 && col < boardSize;
+}
+
+function createGrid() {
+  board.style.gridTemplateColumns = `repeat(${boardSize}, 1fr)`;
+  getGemsPositions();
+  hints = getHintsPositions();
+
+  for (let row = 0; row < boardSize; row++) {
+    for (let col = 0; col < boardSize; col++) {
+      const cell = new Cell(row, col, onGemFound);
+      cells.push(cell);
+      if (gemPositions.includes(`${row}-${col}`)) {
+        cell.setType("gem");
+      }
+      if (hints.includes(`${row}-${col}`)) {
+        cell.setType("hint");
+      }
+    }
+  }
+}
+
+function clearGrid() {
+  while (board.firstChild) {
+    board.innerHTML = "";
+  }
+}
+
+function setUpGame() {
+  resetGameState();
+  createGrid();
+  cells.forEach((cell) => cell.setEnabled());
+  gameOverEl.forEach((el) => hideElement(el));
+  cells.forEach((cell) => cell.setDisabled());
+  showElement(actions);
+  showElement(startButton);
+}
+
+function startGame() {
+  hideElement(startButton);
+  cells.forEach((cell) => cell.setEnabled());
+  startTimer();
+}
+
+function resetGameState() {
+  clearGrid();
+  timeLeft = TIMER;
+  timerValue.textContent = TIMER;
+  gemPositions = [];
+  totalScore = 0;
+  gemsFound = 0;
+  scoreEl.textContent = totalScore;
+  gemEl.textContent = gemsFound;
+  isPaused = false;
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timeLeft--;
+    timerValue.textContent = timeLeft;
+    if (timeLeft === 0 && gemsFound < gemCount) {
+      clearInterval(timerInterval);
+      showElement(youLostEl);
+      cells.forEach((cell) => cell.setDisabled());
+      hideElement(actions);
+    }
+  }, 1000);
+}
+
+function handlePause() {
+  if (isPaused) {
+    isPaused = false;
+    startTimer();
+    cells.forEach((cell) => cell.setEnabled());
+    pauseButton.style.backgroundImage = "url('./assets/icons/icon-pause.png')";
+  } else {
+    clearInterval(timerInterval);
+    isPaused = true;
+    cells.forEach((cell) => cell.setDisabled());
+    pauseButton.style.backgroundImage = "url('./assets/icons/icon-play-s.png')";
+  }
+}
+
+function handleLevelUp() {
+  boardSize += 1;
+  gemCount += 1;
+  setUpGame();
+}
+
+function onGemFound() {
+  totalScore += 100;
+  scoreEl.textContent = totalScore;
+  gemsFound++;
+  gemEl.textContent = gemsFound;
+  if (gemsFound === gemCount && timeLeft > 0) {
+    clearInterval(timerInterval);
+    showElement(congratulationsEl);
+
+    scoreEl.textContent = getFinalScore();
+  }
+}
+
+function getFinalScore() {
+  return totalScore + timeLeft * 10;
+}
+
+function showElement(element) {
+  element.classList.remove("hidden");
+}
+
+function hideElement(element) {
+  element.classList.add("hidden");
+}
+
+setUpGame();
